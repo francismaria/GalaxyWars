8b7889bfd071fe1c94a50ab5b5c820a16a2ff2e6
Unknown <franciscomaria97@hotmail.com>
2018-04-15 10:35:46 +0100

    Initial struct created

---
+++ struct/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.GalaxyWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "GALAXY WARS";
+		config.width = 480;
+		config.height = 320;
+		new LwjglApplication(new GalaxyWars(), config);
+	}
+}
